bd55bc522a34f26b5ae7ab710b215a230a3c27ca
Felipe Jardim <jardim.felipe@hotmail.com>
2024-07-15 14:27:01 -0300

    feat: create invest section

---
+++ components/pages/index/invest.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup></script>
+
+<template>
+  <section id="invest-page">
+    <div
+      class="flex flex-col w-full justify-center items-center gap-4 lg:gap-8"
+    >
+      <h2 class="text-2xl">Get in touch</h2>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@ import HomeHeroSection from "~/components/pages/index/hero.vue";
 import HomeAboutSection from "~/components/pages/index/about.vue";
 import HomeProductSection from "~/components/pages/index/product.vue";
 import HomeLearnSection from "~/components/pages/index/learn.vue";
+import InvestSection from "~/components/pages/index/invest.vue";
 
 useHead({
   title: "Home",
@@ -15,6 +16,7 @@ useHead({
     <home-about-section class="max-w-7xl mx-auto" />
     <home-product-section class="max-w-7xl mx-auto" />
     <home-learn-section class="max-w-7xl mx-auto" />
+    <invest-section class="max-w-7xl mx-auto" />
   </main>
 </template>
 

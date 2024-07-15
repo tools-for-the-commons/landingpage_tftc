a54ecbc561222f5fa35d8f763d0d6d03b0a71d0c
Felipe Jardim <jardim.felipe@hotmail.com>
2024-07-15 20:01:38 -0300

    feat: add subtitles and increase sections size

--- components/pages/index/build.vue
+++ components/pages/index/build.vue
@@ -10,35 +10,54 @@ import TopVoices from "~/assets/images/build/Top Voices.png";
 <template>
   <section
     id="home-about-page"
-    class="flex flex-col items-center justify-center gap-8 px-4 lg:px-8 xl:px-32 py-8 bg-zinc-50 dark:bg-slate-900"
+    class="min-h-[32rem] flex flex-col items-center justify-center gap-8 px-4 lg:px-8 xl:px-32 py-8 bg-zinc-50 dark:bg-slate-900"
   >
-    <h3 class="text-center text-3xl font-bold">Build</h3>
+    <div class="flex flex-col gap-2">
+      <h3 class="text-center text-3xl font-bold">Build</h3>
+      <span class="text-muted-foreground text-center">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lacus
+        nulla, rutrum nec venenatis sit amet, auctor in lectus. Nulla facilisi.
+        Cras sed tortor vel augue molestie porttitor volutpat eget lacus.
+      </span>
+    </div>
     <ui-horizontal-list
       class="w-full"
       :logos="[
         {
           name: 'Blockchain Developers',
           url: BlockChainDevelopers,
+          description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lacus
+        Cras lacus.`,
         },
         {
           name: 'Creators',
           url: Creators,
+          description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lacus
+        Cras lacus.`,
         },
         {
           name: 'Designers',
           url: Designers,
+          description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lacus
+        Cras lacus.`,
         },
         {
           name: 'Key Opinion Leaders',
           url: KeyOpinionLeaders,
+          description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lacus
+        Cras lacus.`,
         },
         {
           name: 'Refugees',
           url: SoftwareEngineers,
+          description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lacus
+        Cras lacus.`,
         },
         {
           name: 'Top Voices',
           url: TopVoices,
+          description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lacus
+        Cras lacus.`,
         },
       ]"
     />

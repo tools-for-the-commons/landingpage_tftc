a95c8b7578c08957ff5a025efe37de42d12fb16d
Felipe Jardim <jardim.felipe@hotmail.com>
2024-07-17 16:31:32 -0300

    feat: add text and image

--- components/pages/index/product.vue
+++ components/pages/index/product.vue
@@ -26,75 +26,75 @@ const items = [
   },
 ];
 
-const selectedItem = ref(items[0]);
-const transitionInterval = ref<NodeJS.Timeout | null>(null);
+// const selectedItem = ref(items[0]);
+// const transitionInterval = ref<NodeJS.Timeout | null>(null);
 
-function handleSelectItem(item: (typeof items)[number]) {
-  selectedItem.value = item;
-  startTransition();
-}
+// function handleSelectItem(item: (typeof items)[number]) {
+//   selectedItem.value = item;
+//   startTransition();
+// }
 
-function startTransition() {
-  if (transitionInterval.value) {
-    endTransition();
-  }
+// function startTransition() {
+//   if (transitionInterval.value) {
+//     endTransition();
+//   }
 
-  transitionInterval.value = setInterval(() => {
-    const index = items.findIndex(
-      (item) => item.label === selectedItem.value.label
-    );
-    selectedItem.value = items[index + 1] || items[0];
-  }, 8 * 1000);
-}
+//   transitionInterval.value = setInterval(() => {
+//     const index = items.findIndex(
+//       (item) => item.label === selectedItem.value.label
+//     );
+//     selectedItem.value = items[index + 1] || items[0];
+//   }, 8 * 1000);
+// }
 
-function endTransition() {
-  if (!transitionInterval.value) {
-    return;
-  }
-  clearInterval(transitionInterval.value);
-}
+// function endTransition() {
+//   if (!transitionInterval.value) {
+//     return;
+//   }
+//   clearInterval(transitionInterval.value);
+// }
 
-if (import.meta.client) {
-  onMounted(startTransition);
-  onUnmounted(endTransition);
-}
+// if (import.meta.client) {
+//   onMounted(startTransition);
+//   onUnmounted(endTransition);
+// }
 </script>
 
 <template>
-  <section
-    id="home-product-page"
-    class="min-h-screen flex flex-col lg:flex-row items-center gap-4 lg:gap-8 p-4 md:px-24"
-  >
-    <div class="flex flex-col w-full max-w-lg">
-      <h3 class="text-lg font-medium">
-        Introducing CommonWorld, a gateway to the future of open governance,
-        digital assets, and digital commerce, facilitating secure data exchange
-        and equitable resource sharing across multiple world regions.
-      </h3>
-      <ul class="mt-4 lg:mt-12 list-disc px-6">
-        <li
-          class="my-2 cursor-pointer"
-          v-for="item of items"
-          :key="item.label"
-          @click="handleSelectItem(item)"
-        >
-          <span class="text-zinc-900 dark:text-zinc-100">
-            {{ item.label }}
-          </span>
-        </li>
-      </ul>
-    </div>
-    <div class="flex lg:mx-auto">
-      <template v-for="item of items">
-        <transition>
-          <img
-            class="product-image flex dark:invert w-full max-w-72 lg:max-w-sm pointer-events-none transition-all"
-            :src="item.image"
-            :alt="item.label"
-            v-if="selectedItem.label === item.label"
-          />
-        </transition>
-      </template>
+  <section id="home-product-page" class="flex flex-col gap-8 my-9">
+    <h3 class="text-center text-3xl font-bold">Use</h3>
+    <div
+      class="flex flex-col lg:flex-row items-center gap-4 lg:gap-8 p-4 md:px-24"
+    >
+      <div class="flex flex-col w-full max-w-lg">
+        <h3 class="text-lg font-medium">
+          Introducing CommonWorld, a gateway to the future of open governance,
+          digital assets, and digital commerce, facilitating secure data
+          exchange and equitable resource sharing across multiple world regions.
+        </h3>
+        <ul class="mt-4 lg:mt-12 list-disc px-6">
+          <li
+            class="my-2 cursor-pointer"
+            v-for="item of items"
+            :key="item.label"
+          >
+            <span class="text-zinc-900 dark:text-zinc-100">
+              {{ item.label }}
+            </span>
+          </li>
+        </ul>
+      </div>
+      <div class="flex lg:mx-auto">
+        <!-- <template v-for="item of items"> -->
+        <!-- <transition> -->
+        <img
+          class="product-image flex w-full max-w-72 lg:max-w-sm pointer-events-none transition-all"
+          src="~assets/images/product/Product_Section.png"
+          alt="CommonWorld"
+        />
+        <!-- </transition> -->
+        <!-- </template> -->
+      </div>
     </div>
   </section>
 </template>

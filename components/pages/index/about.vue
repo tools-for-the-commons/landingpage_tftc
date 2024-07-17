8f5538f0e35f0e2c14f7576a35d6220c7c162742
Felipe Jardim <jardim.felipe@hotmail.com>
2024-07-17 16:19:36 -0300

    feat: add manifesto button

--- components/pages/index/about.vue
+++ components/pages/index/about.vue
@@ -5,13 +5,18 @@
     id="home-about-page"
     class="min-h-[32rem] flex flex-col md:flex-row items-center justify-center gap-1 px-4 lg:px-8 xl:px-32 py-8 bg-zinc-50 dark:bg-slate-900"
   >
-    <div class="flex flex-col">
+    <div class="flex flex-col gap-8">
       <h3 class="text-center text-3xl font-bold">What is TftC?</h3>
-      <h4 class="text-center text-lg font-normal max-w-2xl mt-8">
+      <h4 class="text-center text-lg font-normal max-w-2xl">
         TftC is at the forefront of open governance solutions, harnessing
         blockchain, distributed public utility and public goods management
         technologies to build network-managed cities and societies.
       </h4>
+      <div class="flex justify-center w-full">
+        <button class="border border-slate-200 dark:border-zinc-600 px-4 py-3">
+          Manifesto
+        </button>
+      </div>
     </div>
   </section>
 </template>
